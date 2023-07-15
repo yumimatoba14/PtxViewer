3544aa0f516f772b747c4ec4367b53a718ef7f0d
yumimatoba14 <yumimatoba14@gmail.com>
2023-07-15 19:04:01 +0900

    added some necessary entities. - added test code to draw a panel. - adding mainView. - implementing file menus.

--- PtxViewer/Form1.Designer.cs
+++ PtxViewer/Form1.Designer.cs
@@ -29,13 +29,82 @@ namespace PtxViewer
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.mainMenuStrip = new System.Windows.Forms.MenuStrip();
+            this.fileMenu = new System.Windows.Forms.ToolStripMenuItem();
+            this.openMenu = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitMenu = new System.Windows.Forms.ToolStripMenuItem();
+            this.mainView = new System.Windows.Forms.Panel();
+            this.mainMenuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // mainMenuStrip
+            // 
+            this.mainMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileMenu});
+            this.mainMenuStrip.Location = new System.Drawing.Point(0, 0);
+            this.mainMenuStrip.Name = "mainMenuStrip";
+            this.mainMenuStrip.Size = new System.Drawing.Size(800, 24);
+            this.mainMenuStrip.TabIndex = 0;
+            this.mainMenuStrip.Text = "menuStrip1";
+            // 
+            // fileMenu
+            // 
+            this.fileMenu.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openMenu,
+            this.exitMenu});
+            this.fileMenu.Name = "fileMenu";
+            this.fileMenu.Size = new System.Drawing.Size(37, 20);
+            this.fileMenu.Text = "&File";
+            // 
+            // openMenu
+            // 
+            this.openMenu.Name = "openMenu";
+            this.openMenu.Size = new System.Drawing.Size(180, 22);
+            this.openMenu.Text = "&open";
+            this.openMenu.Click += new System.EventHandler(this.openMenu_Click);
+            // 
+            // exitMenu
+            // 
+            this.exitMenu.Name = "exitMenu";
+            this.exitMenu.Size = new System.Drawing.Size(180, 22);
+            this.exitMenu.Text = "&exit";
+            this.exitMenu.Click += new System.EventHandler(this.exitMenu_Click);
+            // 
+            // mainView
+            // 
+            this.mainView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.mainView.Location = new System.Drawing.Point(0, 27);
+            this.mainView.Name = "mainView";
+            this.mainView.Size = new System.Drawing.Size(800, 424);
+            this.mainView.TabIndex = 1;
+            this.mainView.Paint += new System.Windows.Forms.PaintEventHandler(this.mainView_Paint);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.mainView);
+            this.Controls.Add(this.mainMenuStrip);
+            this.MainMenuStrip = this.mainMenuStrip;
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.mainMenuStrip.ResumeLayout(false);
+            this.mainMenuStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip mainMenuStrip;
+        private System.Windows.Forms.ToolStripMenuItem fileMenu;
+        private System.Windows.Forms.ToolStripMenuItem openMenu;
+        private System.Windows.Forms.ToolStripMenuItem exitMenu;
+        private System.Windows.Forms.Panel mainView;
     }
 }
 
--- PtxViewer/Form1.cs
+++ PtxViewer/Form1.cs
@@ -16,5 +16,29 @@ namespace PtxViewer
         {
             InitializeComponent();
         }
+
+        private void exitMenu_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void openMenu_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Filter = "PTX File(*.ptx)|*.ptx|All file(*.*)|*.*";
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    MessageBox.Show(openFileDialog.FileName, "開発中", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+        }
+
+        private void mainView_Paint(object sender, PaintEventArgs e)
+        {
+            var panelSize = mainView.Size;
+            int margin = 1;
+            e.Graphics.DrawRectangle(System.Drawing.Pens.Green, margin, margin, panelSize.Width - margin * 2, panelSize.Height - margin * 2);
+        }
     }
 }
