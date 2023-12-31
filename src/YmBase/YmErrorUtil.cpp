a661d1cafb759ee93c5a7f2f36534f3f9d71e8f3
yumimatoba14 <yumimatoba14@gmail.com>
2023-12-31 20:59:14 +0900

    modified to output messages to output window.

--- src/YmBase/YmErrorUtil.cpp
+++ src/YmBase/YmErrorUtil.cpp
@@ -1,12 +1,15 @@
 #include "pch.h"
 #include "YmErrorUtil.h"
 #include <exception>
+#include "YmDebugOutputStream.h"
 
 using namespace std;
 using namespace Ymcpp;
 
 void YmErrorUtil::WriteLog(const char* pMsg, const char* pFile, int srcLine)
 {
+	YmDebugOutputStream out;
+	out << pFile << "(" << srcLine << ") : " << pMsg << endl;
 }
 
 void YmErrorUtil::ThrowError(const char* pMsg, const char* pFile, int srcLine)
